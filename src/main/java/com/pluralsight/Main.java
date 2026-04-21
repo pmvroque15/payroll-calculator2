ae370224dd2cd4cc2c4e8e92cf37375ca7ad1f91
pmvroque15 <pmvroque@gmail.com>
2026-04-21 14:08:29 -0400

    Added bufwriter and filewriter to make a new file

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -14,15 +14,19 @@ public class Main {
             //prompts for the employee/user
             System.out.println("Enter the name of the file employee file to process: ");
             String file = scanner.nextLine();
+
+            FileReader fileReader = new FileReader("src/main/resources/employees.csv");
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+
             FileWriter fileWriter = new FileWriter(file);
             BufferedWriter bufWriter = new BufferedWriter(fileWriter);
 
-            bufWriter.close();
-            Employee[] employees = new Employee[8];
+//            Employee[] employees = new Employee[8];
             int index = 0;
             String line;
             int employeeNumber = 1;
-            while ((line = (bufWriter.readLine())) != null) {
+
+            while ((line = (bufferedReader.readLine())) != null) {
                 String[] parts = line.split("\\|");
                 if (line.startsWith("id")) {
                     continue;
@@ -40,21 +44,33 @@ public class Main {
                 newEmployee.setPayRate(payRate);
 
                 //Adding in arrays of employees
-                employees[index] = newEmployee;
-                index++;
-
-                System.out.println("===============================");
-                System.out.printf("      Employee #%s Details        \n", employeeNumber++);
-                System.out.println("===============================");
-                System.out.printf("Employee ID: %s\n", newEmployee.getId());
-                System.out.printf("Employee Name: %s\n", newEmployee.getName());
-                System.out.printf("Hours worked: %s\n", newEmployee.getHoursWorked());
-                System.out.printf("Pay Rate: $%s/hour\n", newEmployee.getPayRate());
-                System.out.printf("Gross Pay Total: $%s\n", newEmployee.calculateGrossPay());
-                System.out.println(" ");
+//                employees[index] = newEmployee;
+//                index++;
+                String output = "===============================\n"
+                        + "Employee #" + employeeNumber++ + "Details\n"
+                        + "===============================\n"
+                        + "Employee ID: \n" + newEmployee.getId()
+                        + "Employee Name: \n" + newEmployee.getName()
+                        + "Hours worked: \n" + newEmployee.getHoursWorked()
+                        + "Pay Rate: \n" + newEmployee.getPayRate()
+                        + "Gross Pay Total: \n" + newEmployee.calculateGrossPay();
+//                System.out.println("===============================");
+//                System.out.printf("      Employee #%s Details        \n", employeeNumber++);
+//                System.out.println("===============================");
+//                System.out.printf("Employee ID: %s\n", newEmployee.getId());
+//                System.out.printf("Employee Name: %s\n", newEmployee.getName());
+//                System.out.printf("Hours worked: %s\n", newEmployee.getHoursWorked());
+//                System.out.printf("Pay Rate: $%s/hour\n", newEmployee.getPayRate());
+//                System.out.printf("Gross Pay Total: $%s\n", newEmployee.calculateGrossPay());
+//                System.out.println(" ");
                 
-                line = bufferedReader.readLine();
+//                line = bufferedReader.readLine();
+
+                bufWriter.write(output);
+                bufWriter.newLine();
             }
+            fileReader.close();
+            bufWriter.close();
         } catch (FileNotFoundException e) {
             System.err.println("The file cannot be read. Check your source path of your file name." );
         } catch (IOException e) {
